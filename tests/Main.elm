87da4ccb1992a65f07089967107736802fe91d7d
Ulric Wilfred <shaman.sir@gmail.com>
2016-09-26 07:50:43 +0200

    rewrite tests to elm-community/elm-test, p.3

--- tests/Main.elm
+++ tests/Main.elm
@@ -1,12 +1,20 @@
-module Main exposing (all)
+port module Main exposing (..)
 
 import Test exposing (..)
+import Test.Runner.Node exposing (run)
+import Json.Encode exposing (Value)
 
 import Expression
 import Statement
 
+main : Program Value
+main =
+    run emit all
+
+port emit : ( String, Value ) -> Cmd msg
+
 all : Test
 all =
-  describe "Ast suite" [ Expression.all
-                       , Statement.all
-                       ]
+   describe "Ast suite" [ Expression.all
+                        , Statement.all
+                        ]
