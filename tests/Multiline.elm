84e8981d122b094c20cd5f23859a58626e0e1d15
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-08-28 22:53:22 +0200

    Create test helpers

--- tests/Multiline.elm
+++ tests/Multiline.elm
@@ -1,66 +1,29 @@
-module Multiline exposing (all)
+module Multiline exposing (application)
 
 import Ast exposing (parseExpression, parseStatement)
 import Ast.BinOp exposing (operators)
 import Ast.Expression exposing (..)
-import Expect exposing (..)
-import String
 import Test exposing (describe, test, Test)
+import Helpers exposing (isApplication, var)
 
 
-var a =
-    Variable [ a ]
-
-
-is : Expression -> String -> Expectation
-is e i =
-    case parseExpression operators (String.trim i) of
-        Ok ( _, _, r ) ->
-            Expect.equal e r
-
-        Err ( _, { position }, es ) ->
-            Expect.fail ("failed to parse: " ++ i ++ " at position " ++ toString position ++ " with errors: " ++ toString es)
-
-
-all : Test
-all =
+application : Test
+application =
     describe "Multiline performance"
         [ test "Application" <|
             \() ->
                 "fn\n a\n b\n c\n d\n e\n f\n g\n h\n i\n j\n k"
-                    |> is
-                        (Application
-                            (Application
-                                (Application
-                                    (Application
-                                        (Application
-                                            (Application
-                                                (Application
-                                                    (Application
-                                                        (Application
-                                                            (Application
-                                                                (Application
-                                                                    (var "fn")
-                                                                    (var "a")
-                                                                )
-                                                                (var "b")
-                                                            )
-                                                            (var "c")
-                                                        )
-                                                        (var "d")
-                                                    )
-                                                    (var "e")
-                                                )
-                                                (var "f")
-                                            )
-                                            (var "g")
-                                        )
-                                        (var "h")
-                                    )
-                                    (var "i")
-                                )
-                                (var "j")
-                            )
-                            (var "k")
-                        )
+                    |> isApplication (var "fn")
+                        [ var "a"
+                        , var "b"
+                        , var "c"
+                        , var "d"
+                        , var "e"
+                        , var "f"
+                        , var "g"
+                        , var "h"
+                        , var "i"
+                        , var "j"
+                        , var "k"
+                        ]
         ]
