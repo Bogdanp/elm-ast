68f66aacf76d602673da49eedb4d8a678a4355b7
Krzysztof Wende <krzysztof.wende@gmail.com>
2017-09-28 00:13:33 +0200

    makeApplication test

--- tests/Multiline.elm
+++ tests/Multiline.elm
@@ -13,50 +13,20 @@ application =
         [ test "Application" <|
             \() ->
                 "fn\n a\n b\n c\n d\n e\n f\n g\n h\n i\n j\n k"
-                    |> isExpression
-                        (Application
-                            (Application
-                                (Application
-                                    (Application
-                                        (Application
-                                            (Application
-                                                (Application
-                                                    (Application
-                                                        (Application
-                                                            (Application
-                                                                (Application
-                                                                    (var "fn" { line = 1, column = 0 })
-                                                                    (var "a" { line = 1, column = 0 })
-                                                                    { line = 1, column = 0 }
-                                                                )
-                                                                (var "b" { line = 2, column = 0 })
-                                                                { line = 2, column = 0 }
-                                                            )
-                                                            (var "c" { line = 3, column = 0 })
-                                                            { line = 3, column = 0 }
-                                                        )
-                                                        (var "d" { line = 4, column = 0 })
-                                                        { line = 4, column = 0 }
-                                                    )
-                                                    (var "e" { line = 5, column = 0 })
-                                                    { line = 5, column = 0 }
-                                                )
-                                                (var "f" { line = 6, column = 0 })
-                                                { line = 6, column = 0 }
-                                            )
-                                            (var "g" { line = 7, column = 0 })
-                                            { line = 7, column = 0 }
-                                        )
-                                        (var "h" { line = 8, column = 0 })
-                                        { line = 8, column = 0 }
-                                    )
-                                    (var "i" { line = 9, column = 0 })
-                                    { line = 9, column = 0 }
-                                )
-                                (var "j" { line = 10, column = 0 })
-                                { line = 10, column = 0 }
-                            )
-                            (var "k" { line = 12, column = 1 })
-                            { line = 12, column = 0 }
-                        )
+                    |> isExpression (makeApplication "fn" ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"])
+
         ]
+
+
+makeApplication : String -> List String -> Expression
+makeApplication name args =
+    let
+        line n =
+            { line = n + 1, column = 0 }
+
+        nextArgs ( index, name ) acc =
+            Application acc (var name <| line index) (line index)
+    in
+        args
+            |> List.indexedMap (,)
+            |> List.foldl nextArgs (var name (line 0))
