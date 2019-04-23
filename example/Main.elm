cc575dad036fab6fd46b22cd0cca9ad0fa11a737
Krzysztof Wende <iraasta@gmail.com>
2019-04-23 15:40:21 +0200

    Revert to master

--- example/Main.elm
+++ example/Main.elm
@@ -4,6 +4,7 @@ import Ast
 import Ast.Expression exposing (..)
 import Ast.Statement exposing (..)
 import Html exposing (..)
+import Html
 import Html.Events exposing (..)
 import Json.Decode as JD
 
@@ -41,9 +42,9 @@ withChild title children =
         ]
 
 
-expression : MExp -> Html Msg
+expression : Expression -> Html Msg
 expression e =
-    case e.e of
+    case e of
         List es ->
             withChild e (List.map expression es)
 
